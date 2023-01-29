86edb0d2546e3ab202edac3ce99f23bb40330309
CirroBuild <cirrobuild10@gmail.com>
2023-01-29 15:30:39 -0800

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FrostiDemo.Models;
+using Microsoft.Azure.Cosmos;
+using System.Net;
+
+namespace FrostiDemo.Controllers;
+
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly CosmosClient _cosmosClient;
+
+    public HomeController(ILogger<HomeController> logger, CosmosClient cosmosClient)
+    {
+        _logger = logger;
+        _cosmosClient = cosmosClient;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<HttpStatusCode> InitializeDatabase()
+    {
+        var dbResp = await _cosmosClient.CreateDatabaseIfNotExistsAsync("cosmicworks");
+        var containerResp = await dbResp.Database.CreateContainerIfNotExistsAsync("products", "/category");
+        return containerResp.StatusCode;
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.frosti.json");
+}
+
+var client = new SecretClient(new Uri(builder.Configuration["KV_ENDPOINT"]), new DefaultAzureCredential());
+
+// New instance of CosmosClient class
+var cosmosConnection = client.GetSecret("CosmosConnection").Value.Value;
+builder.Services.AddSingleton(s =>
+{
+    return new CosmosClient(cosmosConnection);
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
